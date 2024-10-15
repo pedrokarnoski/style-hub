63f3dd333cba537dd3fe6784a05023bdd949da6f
pedrokarnoski@gmail.com <pedrokarnoski@gmail.com>
2024-10-15 15:47:04 -0300

    fix: Checkout

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -7,7 +7,7 @@ import { Stripe } from 'stripe'
 import { Confetti } from '@/components/confetti'
 import { Header } from '@/components/header'
 import { Button } from '@/components/ui/button'
-import { MagicCard } from '@/components/ui/magic-card'
+import { Card } from '@/components/ui/card'
 import Particles from '@/components/ui/particles'
 import { stripe } from '@/lib/stripe'
 
@@ -24,7 +24,7 @@ export async function generateMetadata({
 
   if (!sessionId) {
     return {
-      title: 'Compra Efetuada | StyleHub',
+      title: 'Compra efetuada | StyleHub',
       description: 'Compra realizada com sucesso na StyleHub.',
       robots: {
         index: false,
@@ -38,12 +38,13 @@ export async function generateMetadata({
       expand: ['line_items.data.price.product'],
     })
 
-  const product = checkoutSession.line_items?.data[0].price
-    ?.product as Stripe.Product
+  const productNames = checkoutSession.line_items?.data
+    .map((item) => item.price.product.name)
+    .join(', ')
 
   return {
-    title: `Compra de ${product.name} | StyleHub`,
-    description: `Você comprou ${product.name}. Seu produto chegará em breve.`,
+    title: `Compra de ${productNames} | StyleHub`,
+    description: `Você comprou ${productNames}. Seu(s) produto(s) chegarão em breve.`,
     robots: {
       index: false,
       follow: false,
@@ -63,13 +64,9 @@ export default async function Success({ searchParams }: SuccessPageProps) {
       expand: ['line_items.data.price.product'],
     })
 
-  const product = checkoutSession.line_items?.data[0].price
-    ?.product as Stripe.Product
-
   return (
     <>
-      <Header />
-
+      <Header showBag={false} />
       <Particles
         className="absolute inset-0"
         quantity={100}
@@ -77,42 +74,45 @@ export default async function Success({ searchParams }: SuccessPageProps) {
         color="#fff"
         refresh
       />
-
       <Confetti />
-
-      <div className="flex min-h-screen flex-col items-center justify-center px-14 text-center lg:px-20">
-        <h1 className="p-4 text-xl font-semibold text-primary">
-          Compra efetuada
-        </h1>
-
-        <div className="flex flex-col items-center space-y-8">
+      <div className="flex min-h-screen flex-col items-center justify-center px-10 text-center lg:px-20">
+        <div className="flex flex-col items-center space-y-8 md:space-y-16">
+          <h1 className="text-2xl font-semibold text-primary md:text-4xl">
+            Compra efetuada!
+          </h1>
+          <div className="flex w-full max-w-6xl flex-col items-center lg:flex-row lg:items-start">
+            {checkoutSession.line_items?.data.map((item, index) => (
+              <Card
+                key={item.id}
+                className={`z-10 cursor-pointer flex-col items-center justify-center whitespace-nowrap rounded-full bg-gradient-to-bl from-zinc-900 to-zinc-950 shadow-2xl ${
+                  index > 0 ? 'relative -mt-16 md:-ml-28 md:mt-0' : ''
+                }`}
+              >
+                {item.price && item.price.product && (
+                  <Image
+                    src={item.price.product.images[0]}
+                    alt={item.price.product.name}
+                    className="h-[150px] w-[150px] md:h-[450px] md:w-[450px]"
+                    width={520} // largura padrão
+                    height={480} // altura padrão
+                    sizes="(max-width: 640px) 150px, (min-width: 641px) 450px" // Definindo tamanhos responsivos
+                  />
+                )}
+              </Card>
+            ))}
+          </div>
           {checkoutSession.customer_details ? (
-            <div>
+            <div className="text-xl">
               <p>
                 Uhuuul <strong>{checkoutSession.customer_details.name}</strong>,
-                sua <strong>{product.name}</strong> chegará em breve. 🚀
+                sua compra chegará em breve! 🚀
               </p>
               <p>Obrigado por comprar com a gente!</p>
             </div>
           ) : (
             <p>Erro: Detalhes do cliente não encontrados.</p>
           )}
-
-          <div className="flex w-full max-w-6xl flex-col items-center lg:flex-row lg:items-start">
-            <MagicCard
-              className="cursor-pointer flex-col items-center justify-center whitespace-nowrap bg-gradient-to-bl from-zinc-900 to-zinc-950 text-4xl shadow-2xl"
-              gradientColor="#262626"
-            >
-              <Image
-                src={product.images[0]}
-                alt={product.name}
-                width={520}
-                height={480}
-              />
-            </MagicCard>
-          </div>
-
-          <Button size="lg" asChild>
+          <Button variant="link" asChild>
             <Link href="/">Voltar ao catálogo</Link>
           </Button>
         </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -30,7 +30,7 @@ import {
 } from '@/components/ui/sheet'
 import { useCart } from '@/context/cart-context'
 
-export function Header() {
+export function Header({ showBag = true }: { showBag: boolean }) {
   const { cartItems, removeFromCart } = useCart()
   const [isOpen, setIsOpen] = useState(false)
 
@@ -62,124 +62,131 @@ export function Header() {
 
   return (
     <div className="absolute z-20 w-full border-b bg-background lg:px-40">
-      <div className="flex h-16 items-center justify-between gap-6 px-6">
-        <Link href="/">
-          <div className="flex items-center gap-3 font-semibold text-foreground">
-            <Shirt className="h-5 w-5 text-muted-foreground" />
-            <span className="flex flex-row text-xl tracking-wide text-primary">
-              Style<p className="text-muted-foreground">Hub</p>
-            </span>
-          </div>
+      <div
+        className={`flex h-16 items-center justify-between gap-6 px-6 ${!showBag ? 'justify-center' : ''}`}
+      >
+        <Link
+          href="/"
+          className={`flex items-center gap-3 font-semibold text-foreground ${!showBag ? 'absolute left-1/2 -translate-x-1/2 transform' : ''}`}
+        >
+          <Shirt className="h-5 w-5 text-muted-foreground" />
+          <span className="flex flex-row text-xl tracking-wide text-primary">
+            Style<p className="text-muted-foreground">Hub</p>
+          </span>
         </Link>
 
-        <Sheet open={isOpen} onOpenChange={setIsOpen}>
-          <SheetTrigger asChild>
-            <div className="relative">
-              <Button size="icon">
-                <ShoppingBasket className="size-6" />
-              </Button>
-              {totalItems > 0 && (
-                <span className="absolute -right-2 -top-2 flex h-6 w-6 items-center justify-center rounded-full bg-white text-sm font-bold text-background">
-                  {totalItems}
-                </span>
-              )}
-            </div>
-          </SheetTrigger>
+        {showBag && (
+          <Sheet open={isOpen} onOpenChange={setIsOpen}>
+            <SheetTrigger asChild>
+              <div className="relative">
+                <Button size="icon">
+                  <ShoppingBasket className="size-6" />
+                </Button>
+                {totalItems > 0 && (
+                  <span className="absolute -right-2 -top-2 flex h-6 w-6 items-center justify-center rounded-full bg-white text-sm font-bold text-background">
+                    {totalItems}
+                  </span>
+                )}
+              </div>
+            </SheetTrigger>
 
-          <SheetContent className="flex h-full flex-col justify-between overflow-y-auto py-10">
-            <SheetHeader>
-              <SheetTitle>Sacola de compras</SheetTitle>
-              <SheetDescription>
-                Aqui estão os itens que você adicionou a sua sacola.
-              </SheetDescription>
-            </SheetHeader>
-            <div className="flex-grow space-y-3">
-              {cartItems.length === 0 ? (
-                <p className="text-center text-muted-foreground">
-                  Seu carrinho está vazio.
-                </p>
-              ) : (
-                cartItems.map((item) => (
-                  <Card
-                    key={item.id}
-                    className="flex flex-col items-start justify-between sm:flex-row"
-                  >
-                    <div className="flex w-full flex-col items-center py-1 sm:flex-row">
-                      <Image
-                        src={item.image}
-                        alt={item.name}
-                        width={100}
-                        height={60}
-                      />
+            <SheetContent className="flex h-full flex-col justify-between overflow-y-auto py-10">
+              <SheetHeader>
+                <SheetTitle>Sacola de compras</SheetTitle>
+                <SheetDescription>
+                  Aqui estão os itens que você adicionou a sua sacola.
+                </SheetDescription>
+              </SheetHeader>
+              <div className="flex-grow space-y-3">
+                {cartItems.length === 0 ? (
+                  <p className="text-center text-muted-foreground">
+                    Seu carrinho está vazio.
+                  </p>
+                ) : (
+                  cartItems.map((item) => (
+                    <Card
+                      key={item.id}
+                      className="flex flex-col items-start justify-between sm:flex-row"
+                    >
+                      <div className="flex w-full flex-col items-center py-1 sm:flex-row">
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={100}
+                          height={60}
+                        />
 
-                      <div className="flex w-full flex-col items-center text-center sm:items-start sm:text-left">
-                        <p className="font-medium">{item.name}</p>
-                        <p className="text-lg font-semibold text-primary">
-                          {item.price}
-                        </p>
-                        <div className="flex items-center justify-between gap-4 lg:w-full">
-                          <span className="text-sm text-muted-foreground">
-                            Quantidade: {item.quantity}
-                          </span>
-                          <AlertDialog>
-                            <AlertDialogTrigger asChild>
-                              <Button
-                                variant="ghost"
-                                size="icon"
-                                className="mr-2 h-8 w-8 text-red-400"
-                              >
-                                <Trash className="h-5 w-5" />
-                              </Button>
-                            </AlertDialogTrigger>
-                            <AlertDialogContent>
-                              <AlertDialogHeader>
-                                <AlertDialogTitle>
-                                  Tem certeza que deseja remover este item?
-                                </AlertDialogTitle>
-                                <AlertDialogDescription>
-                                  Vai remover {item.name} da sua sacola? 🥹
-                                </AlertDialogDescription>
-                              </AlertDialogHeader>
-                              <AlertDialogFooter>
-                                <AlertDialogCancel>Cancelar</AlertDialogCancel>
-                                <AlertDialogAction
-                                  onClick={() => removeFromCart(item.id)}
+                        <div className="flex w-full flex-col items-center text-center sm:items-start sm:text-left">
+                          <p className="font-medium">{item.name}</p>
+                          <p className="text-lg font-semibold text-primary">
+                            {item.price}
+                          </p>
+                          <div className="flex items-center justify-between gap-4 lg:w-full">
+                            <span className="text-sm text-muted-foreground">
+                              Quantidade: {item.quantity}
+                            </span>
+                            <AlertDialog>
+                              <AlertDialogTrigger asChild>
+                                <Button
+                                  variant="ghost"
+                                  size="icon"
+                                  className="mr-2 h-8 w-8 text-red-400"
                                 >
-                                  Remover
-                                </AlertDialogAction>
-                              </AlertDialogFooter>
-                            </AlertDialogContent>
-                          </AlertDialog>
+                                  <Trash className="h-5 w-5" />
+                                </Button>
+                              </AlertDialogTrigger>
+                              <AlertDialogContent>
+                                <AlertDialogHeader>
+                                  <AlertDialogTitle>
+                                    Tem certeza que deseja remover este item?
+                                  </AlertDialogTitle>
+                                  <AlertDialogDescription>
+                                    Vai remover {item.name} da sua sacola? 🥹
+                                  </AlertDialogDescription>
+                                </AlertDialogHeader>
+                                <AlertDialogFooter>
+                                  <AlertDialogCancel>
+                                    Cancelar
+                                  </AlertDialogCancel>
+                                  <AlertDialogAction
+                                    onClick={() => removeFromCart(item.id)}
+                                  >
+                                    Remover
+                                  </AlertDialogAction>
+                                </AlertDialogFooter>
+                              </AlertDialogContent>
+                            </AlertDialog>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </Card>
-                ))
-              )}
-            </div>
-
-            <Separator />
-
-            {cartItems.length > 0 && (
-              <div className="px-1">
-                <div className="flex flex-row items-center justify-between">
-                  <p className="text-sm">Quantidade:</p>
-                  <p className="text-lg font-medium">{totalItems}</p>
-                </div>
-                <div className="flex flex-row items-center justify-between">
-                  <p className="font-semibold">Total:</p>
-                  <p className="text-2xl font-semibold text-primary">
-                    {formattedTotalPrice}
-                  </p>
-                </div>
+                    </Card>
+                  ))
+                )}
               </div>
-            )}
 
-            <Separator />
+              <Separator />
 
-            {cartItems.length > 0 && <BuyProduct />}
-          </SheetContent>
-        </Sheet>
+              {cartItems.length > 0 && (
+                <div className="px-1">
+                  <div className="flex flex-row items-center justify-between">
+                    <p className="text-sm">Quantidade:</p>
+                    <p className="text-lg font-medium">{totalItems}</p>
+                  </div>
+                  <div className="flex flex-row items-center justify-between">
+                    <p className="font-semibold">Total:</p>
+                    <p className="text-2xl font-semibold text-primary">
+                      {formattedTotalPrice}
+                    </p>
+                  </div>
+                </div>
+              )}
+
+              <Separator />
+
+              {cartItems.length > 0 && <BuyProduct />}
+            </SheetContent>
+          </Sheet>
+        )}
       </div>
     </div>
   )
